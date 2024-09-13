b55e3f593d60a558ce532002f6c751a0ec6bf2b8
Gustavo Rafael Nunes Durães <gustavo.ipatinga@gmail.com>
2024-09-13 16:09:37 -0300

    coisas do João

--- Atividade03/app/src/main/java/com/andresc/atividade03/Cadastrarproduto.java
+++ Atividade03/app/src/main/java/com/andresc/atividade03/Cadastrarproduto.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class    CadastrarCliente extends AppCompatActivity {
+public class Cadastrarproduto extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastrar_cliente);
+        setContentView(R.layout.activity_cadastrarproduto);
     }
 }
